f781eba40b2fefe4d21a573f6bf7ff88ad5afb3f
romantulchak <romantulchak29@gmail.com>
2022-06-22 20:00:14 +0300

    Initialize project

---
+++ src/main/java/com/notekeep/notekeep/NoteKeepApplication.java
@@ -0,0 +1,13 @@
+package com.notekeep.notekeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteKeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteKeepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notekeep/notekeep/NoteKeepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notekeep.notekeep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteKeepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
